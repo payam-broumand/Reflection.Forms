98b9721a0c7b6e7704287a1a78709eb2218618ed
Payam <payam.broumandi@gmail.com>
2021-04-08 17:02:42 +0430

    Add Form and design ui to display MetaData Type information

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtMetaData = new System.Windows.Forms.TextBox();
+            this.btnLoadMetaData = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtMetaData
+            // 
+            this.txtMetaData.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMetaData.Location = new System.Drawing.Point(12, 12);
+            this.txtMetaData.Multiline = true;
+            this.txtMetaData.Name = "txtMetaData";
+            this.txtMetaData.Size = new System.Drawing.Size(446, 384);
+            this.txtMetaData.TabIndex = 0;
+            // 
+            // btnLoadMetaData
+            // 
+            this.btnLoadMetaData.Location = new System.Drawing.Point(164, 402);
+            this.btnLoadMetaData.Name = "btnLoadMetaData";
+            this.btnLoadMetaData.Size = new System.Drawing.Size(123, 36);
+            this.btnLoadMetaData.TabIndex = 1;
+            this.btnLoadMetaData.Text = "Load DataType Info";
+            this.btnLoadMetaData.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(572, 450);
+            this.ClientSize = new System.Drawing.Size(467, 450);
+            this.Controls.Add(this.btnLoadMetaData);
+            this.Controls.Add(this.txtMetaData);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtMetaData;
+        private System.Windows.Forms.Button btnLoadMetaData;
     }
 }
 
