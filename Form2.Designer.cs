f89f16391eb3887e7b70cba048cb7dd69486e58b
Payam <payam.broumandi@gmail.com>
2021-04-08 23:06:24 +0430

    Get Assembly Data Types and MetaType

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -30,55 +30,70 @@
         {
             this.btnLoadAssembly = new System.Windows.Forms.Button();
             this.txtAssemblyName = new System.Windows.Forms.TextBox();
-            this.listAssemblyDataTypes = new System.Windows.Forms.ListView();
-            this.listAssemblyMetaData = new System.Windows.Forms.ListView();
+            this.listAssemblyDataTypes = new System.Windows.Forms.ListBox();
+            this.txtMetaData = new System.Windows.Forms.TextBox();
+            this.btnCreateInstance = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnLoadAssembly
             // 
-            this.btnLoadAssembly.Location = new System.Drawing.Point(557, 12);
+            this.btnLoadAssembly.Location = new System.Drawing.Point(449, 12);
             this.btnLoadAssembly.Name = "btnLoadAssembly";
             this.btnLoadAssembly.Size = new System.Drawing.Size(102, 36);
             this.btnLoadAssembly.TabIndex = 0;
             this.btnLoadAssembly.Text = "Get Assembly";
             this.btnLoadAssembly.UseVisualStyleBackColor = true;
+            this.btnLoadAssembly.Click += new System.EventHandler(this.btnLoadAssembly_Click);
             // 
             // txtAssemblyName
             // 
+            this.txtAssemblyName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtAssemblyName.Location = new System.Drawing.Point(12, 12);
             this.txtAssemblyName.Multiline = true;
             this.txtAssemblyName.Name = "txtAssemblyName";
-            this.txtAssemblyName.Size = new System.Drawing.Size(539, 36);
+            this.txtAssemblyName.Size = new System.Drawing.Size(431, 36);
             this.txtAssemblyName.TabIndex = 1;
             // 
             // listAssemblyDataTypes
             // 
-            this.listAssemblyDataTypes.HideSelection = false;
+            this.listAssemblyDataTypes.FormattingEnabled = true;
             this.listAssemblyDataTypes.Location = new System.Drawing.Point(12, 54);
             this.listAssemblyDataTypes.Name = "listAssemblyDataTypes";
-            this.listAssemblyDataTypes.Size = new System.Drawing.Size(316, 384);
+            this.listAssemblyDataTypes.Size = new System.Drawing.Size(269, 381);
             this.listAssemblyDataTypes.TabIndex = 2;
-            this.listAssemblyDataTypes.UseCompatibleStateImageBehavior = false;
+            this.listAssemblyDataTypes.SelectedIndexChanged += new System.EventHandler(this.listAssemblyDataTypes_SelectedIndexChanged);
             // 
-            // listAssemblyMetaData
+            // txtMetaData
             // 
-            this.listAssemblyMetaData.HideSelection = false;
-            this.listAssemblyMetaData.Location = new System.Drawing.Point(334, 54);
-            this.listAssemblyMetaData.Name = "listAssemblyMetaData";
-            this.listAssemblyMetaData.Size = new System.Drawing.Size(325, 384);
-            this.listAssemblyMetaData.TabIndex = 2;
-            this.listAssemblyMetaData.UseCompatibleStateImageBehavior = false;
+            this.txtMetaData.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMetaData.Location = new System.Drawing.Point(287, 54);
+            this.txtMetaData.Multiline = true;
+            this.txtMetaData.Name = "txtMetaData";
+            this.txtMetaData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMetaData.Size = new System.Drawing.Size(372, 381);
+            this.txtMetaData.TabIndex = 1;
+            // 
+            // btnCreateInstance
+            // 
+            this.btnCreateInstance.Location = new System.Drawing.Point(557, 12);
+            this.btnCreateInstance.Name = "btnCreateInstance";
+            this.btnCreateInstance.Size = new System.Drawing.Size(102, 36);
+            this.btnCreateInstance.TabIndex = 0;
+            this.btnCreateInstance.Text = "Create Instance";
+            this.btnCreateInstance.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(671, 450);
-            this.Controls.Add(this.listAssemblyMetaData);
             this.Controls.Add(this.listAssemblyDataTypes);
+            this.Controls.Add(this.txtMetaData);
             this.Controls.Add(this.txtAssemblyName);
+            this.Controls.Add(this.btnCreateInstance);
             this.Controls.Add(this.btnLoadAssembly);
             this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -89,7 +104,8 @@
 
         private System.Windows.Forms.Button btnLoadAssembly;
         private System.Windows.Forms.TextBox txtAssemblyName;
-        private System.Windows.Forms.ListView listAssemblyDataTypes;
-        private System.Windows.Forms.ListView listAssemblyMetaData;
+        private System.Windows.Forms.ListBox listAssemblyDataTypes;
+        private System.Windows.Forms.TextBox txtMetaData;
+        private System.Windows.Forms.Button btnCreateInstance;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using MetaData;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,36 @@ namespace Forms
         public Form2()
         {
             InitializeComponent();
+            btnCreateInstance.Enabled = false;
         }
+
+        private void btnLoadAssembly_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Open Assembly ...";
+            openFileDialog.Filter = ".Net Assemblies (*.exe, *.dll)|*.exe;*.dll";
+            DialogResult dialogResult = openFileDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                Assembly assembly = Assembly.LoadFile(openFileDialog.FileName);
+                txtAssemblyName.Text = assembly.ToString();
+                Type[] types = assembly.GetTypes();
+                foreach (var item in types)
+                {
+                    listAssemblyDataTypes.Items.Add(item);
+                }
+                if(types.Length > 0)
+                    btnCreateInstance.Enabled = true;
+            }
+        }
+
+        private void listAssemblyDataTypes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selectedItem = listAssemblyDataTypes.SelectedItem;
+            LoadMetaData metaData = new LoadMetaData((Type)selectedItem);
+            txtMetaData.Text = metaData.LoadGeneralTypeInfo();
+            txtMetaData.Text += metaData.LoadProperties();
+            txtMetaData.Text += metaData.LoadMethods();
+        } 
     }
 }
